6a890ad3b736d705478f055cc13cc5e03cf46c7b
Mannomann <krivmann00@gmail.com>
2023-02-15 23:37:24 +0200

    длинна числа влияет на массив

--- Add to Array-Form of Integer/app.js
+++ Add to Array-Form of Integer/app.js
@@ -4,13 +4,8 @@ function arrToInt (num, k){
     let temp = [...'' + k].map(Number).reverse()
     const result = [...num].reverse().map((el,i) => { // [0] 1
         if(el + (temp[i] | 0)  + counter >= 10){
-            if(counter !== 0) {
-
-            } else {
-
-            }
             res = (el + (temp[i] | 0)  + counter) % 10
-            counter ? counter = 0 : counter = 1
+            counter = 1
             return res
         } else {
             res = el + (temp[i] | 0) + counter
@@ -21,4 +16,4 @@ function arrToInt (num, k){
     return counter ? [1,...result] : result
 }
 
-console.log(arrToInt([9,9,9,9,9,9,9,9,9,9],1))
+console.log(arrToInt([0],2,3))
